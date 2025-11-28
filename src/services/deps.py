b410940005c8c44328ac4565c91c54fbb5da278b
andreism <markoandreis07@gmail.com>
2025-11-28 16:51:47 +0100

    WIP: fetching actual clubs data from Sofascore

--- src/services/deps.py
+++ src/services/deps.py
@@ -2,12 +2,20 @@ from typing import Annotated
 
 from fastapi import Depends
 
+from adapters.sofascore_client import SofascoreClient
 from adapters.sofascore_club_repository import SofascoreClubRepository
 from domain.ports.club_repository import ClubRepository
+from domain.ports.http_client import HttpClient
 from services.clubs import ClubService
 
-def get_club_repository() -> ClubRepository:
-    return SofascoreClubRepository()
+
+def get_http_client() -> HttpClient:
+    return SofascoreClient()
+
+def get_club_repository(
+        http_client: Annotated[HttpClient, Depends(get_http_client)]
+) -> ClubRepository:
+    return SofascoreClubRepository(http_client)
 
 
 
