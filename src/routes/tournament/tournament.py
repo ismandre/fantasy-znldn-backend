e6a4e7a2575473c959e230579135d0b4ef4a8d25
andreism <markoandreis07@gmail.com>
2025-11-29 15:27:26 +0100

    Add actual implementation for tournaments info endpoint

--- src/routes/tournament/tournament.py
+++ src/routes/tournament/tournament.py
@@ -1,16 +1,28 @@
-from fastapi import APIRouter, status
+from typing import Annotated
 
+from fastapi import APIRouter, Depends, HTTPException, status
 
+from domain.errors.tournament import TournamentNotFoundError
+from serializers.tournament import TournamentSerializer
+from services.deps import get_tournament_service
+from services.tournament import TournamentService
 
 router = APIRouter(prefix="/tournament", tags=["tournament"])
 
 
 
 @router.get(
-    path="/",
+    path="/{tournament_id}",
     name="Get tournament info",
     response_model=TournamentSerializer,
     status_code=status.HTTP_200_OK
 )
-async def get_tournament_info():
-    return {}
+async def get_tournament_info(
+        tournament_id: int,
+        service: Annotated[TournamentService, Depends(get_tournament_service)]
+) -> TournamentSerializer:
+    try:
+        tournament = await service.get_tournament(tournament_id)
+    except TournamentNotFoundError as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e)) from e
+    return TournamentSerializer.model_validate(tournament)
