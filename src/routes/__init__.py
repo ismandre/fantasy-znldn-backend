dc1f9d186b2c1163a69676855b8ffd77bf9f1e82
andreism <markoandreis07@gmail.com>
2025-11-29 15:27:43 +0100

    Include tournaments route

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -2,11 +2,13 @@ from fastapi import APIRouter
 
 from routes.clubs.clubs import router as clubs_router
 from routes.dummy.dummy import router as dummy_router
+from routes.tournament.tournament import router as tournament_router
 
 router = APIRouter()
 
 
 router.include_router(clubs_router)
 router.include_router(dummy_router)
+router.include_router(tournament_router)
 
 __all__ = ["router"]
