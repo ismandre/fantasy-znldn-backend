5812c3f20d6b5aad39c9b65addee73cf5f68dd4a
andreism <markoandreis07@gmail.com>
2025-11-29 20:37:34 +0100

    Add tournament fetching logic to service layer

--- src/services/tournament.py
+++ src/services/tournament.py
@@ -1,4 +1,4 @@
-from domain.models.tournament import Tournament
+from domain.models.tournament import Tournament, TournamentSeason
 from domain.ports.tournament_repository import TournamentRepository
 
 
@@ -8,3 +8,6 @@ class TournamentService:
 
     async def get_tournament(self, tournament_id: int) -> Tournament:
         return await self.tournament_repository.get(tournament_id)
+
+    async def get_tournament_seasons(self, tournament_id: int) -> list[TournamentSeason]:
+        return await self.tournament_repository.get_tournament_seasons(tournament_id)
