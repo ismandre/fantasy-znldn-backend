dff54c4668291fffa3f835dd47baa993963741ba
andreism <markoandreis07@gmail.com>
2025-11-30 11:57:55 +0100

    Add domain models for event incidents

--- src/domain/models/event.py
+++ src/domain/models/event.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
+from enum import StrEnum
 
+from domain.models.player import Player
 from domain.models.round import TournamentRound
 from domain.models.score import Score
 from domain.models.team import Team
@@ -10,6 +12,7 @@ from domain.models.tournament import Tournament, TournamentSeason
 class EventStatus:
     description: str
     type: str
+
 
 @dataclass
 class Event:
@@ -22,3 +25,57 @@ class Event:
     start_timestamp: int
     status: EventStatus
     round_info: TournamentRound
+
+
+@dataclass
+class IncidentType(StrEnum):
+    PERIOD = "period"
+    SUBSTITUTION = "substitution"
+    GOAL = "goal"
+    CARD = "card"
+
+
+@dataclass
+class EventIncident:
+    id: int
+    time: int
+    incident_type: IncidentType
+
+
+@dataclass
+class PeriodType(StrEnum):
+    HALF_TIME = "HT"
+    FULL_TIME = "FT"
+
+
+@dataclass
+class PeriodEventIncident(EventIncident):
+    text: PeriodType
+    home_score: int
+    away_score: int
+
+
+@dataclass
+class SubstitutionEventIncident(EventIncident):
+    player_in: Player
+    player_out: Player
+
+
+@dataclass
+class GoalEventIncident(EventIncident):
+    home_score: int
+    away_score: int
+    player: Player
+
+
+@dataclass
+class CardType(StrEnum):
+    YELLOW = "yellow"
+    YELLOW_RED = "yellowRed"
+    RED = "red"
+
+
+@dataclass
+class CardEventIncident(EventIncident):
+    player: Player
+    card: CardType
