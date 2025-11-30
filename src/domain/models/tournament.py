c5288503e7f3da93d115108c4ae1e0e91d518979
andreism <markoandreis07@gmail.com>
2025-11-30 10:15:47 +0100

    Make has_rounds and start_date optional

--- src/domain/models/tournament.py
+++ src/domain/models/tournament.py
@@ -1,12 +1,13 @@
 from dataclasses import dataclass
+from typing import Optional
 
 
 @dataclass
 class Tournament:
     id: int
     name: str
-    has_rounds: bool
-    start_date: int
+    has_rounds: Optional[bool] = None
+    start_date: Optional[int] = None
 
 @dataclass
 class TournamentSeason:
