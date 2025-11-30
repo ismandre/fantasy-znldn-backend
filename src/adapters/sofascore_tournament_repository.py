6b7343e38c8cd4e76aab3f1d7f83869cafd3b51e
andreism <markoandreis07@gmail.com>
2025-11-30 10:15:22 +0100

    Extend tournament repository interface and implement fetching actual data about round events from Sofascore

--- src/adapters/sofascore_tournament_repository.py
+++ src/adapters/sofascore_tournament_repository.py
@@ -1,10 +1,13 @@
 from typing import cast, override
 
 from adapters.types.sofascore_tournaments import UniqueTournamentResponse, UniqueTournamentSeasonsResponse, \
-    UniqueTournamentSeasonRoundsResponse
+    UniqueTournamentSeasonRoundsResponse, UniqueTournamentSeasonEventsRoundResponse
 from domain.errors.http_client import HTTPClientError
 from domain.errors.tournament import TournamentNotFoundError
+from domain.models.event import Event, EventStatus
 from domain.models.round import TournamentRound
+from domain.models.score import Score
+from domain.models.team import Team
 from domain.models.tournament import Tournament, TournamentSeason
 from domain.ports.http_client import HttpClient
 from domain.ports.tournament_repository import TournamentRepository
@@ -77,6 +80,70 @@ class SofascoreTournamentRepository(TournamentRepository):
             print(f"Unexpected error when fetching info for tournament {tournament_id}: {e}")
             raise
         return self._map_response_to_tournament_season_rounds_model(response_data)
+
+    @override
+    async def get_tournament_round_events(self, tournament_id: int, season_id: int, round_number: int) -> list[Event]:
+        try:
+            response = await self.http_client.get(f"https://api.sofascore.com/api/v1/unique-tournament/{tournament_id}/season/{season_id}/events/round/{round_number}")
+            if "error" in response:
+                if response["error"].get("code") == 404:
+                    raise TournamentNotFoundError(f"Tournament with ID {tournament_id} not found")
+            response_data = cast(
+                UniqueTournamentSeasonEventsRoundResponse,
+                response
+            )
+        except HTTPClientError as e:
+            print(f"HTTP error when fetching info for tournament {tournament_id}: {e}")
+            raise
+        except Exception as e:
+            print(f"Unexpected error when fetching info for tournament {tournament_id}: {e}")
+            raise
+        return self._map_response_to_tournament_season_round_events_model(response_data)
+
+    def _map_response_to_tournament_season_round_events_model(self, response_data: UniqueTournamentSeasonEventsRoundResponse) -> list[Event]:
+        return [
+            Event(
+                tournament=Tournament(
+                    id=event["tournament"]["id"],
+                    name=event["tournament"]["name"]
+                ),
+                season=TournamentSeason(
+                    id=event["season"]["id"],
+                    name=event["season"]["name"],
+                    year=event["season"]["year"]
+                ),
+                home_team=Team(
+                    id=event["homeTeam"]["id"],
+                    name=event["homeTeam"]["name"]
+                ),
+                away_team=Team(
+                    id=event["awayTeam"]["id"],
+                    name=event["awayTeam"]["name"]
+                ),
+                home_score=Score(
+                    current=event["homeScore"]["current"],
+                    display=event["homeScore"]["display"],
+                    period1=event["homeScore"]["period1"],
+                    period2=event["homeScore"]["period2"],
+                    normaltime=event["homeScore"]["normaltime"]
+                ),
+                away_score=Score(
+                    current=event["awayScore"]["current"],
+                    display=event["awayScore"]["display"],
+                    period1=event["awayScore"]["period1"],
+                    period2=event["awayScore"]["period2"],
+                    normaltime=event["awayScore"]["normaltime"]
+                ),
+                start_timestamp=event["startTimestamp"],
+                status=EventStatus(
+                    description=event["status"]["description"],
+                    type=event["status"]["type"]
+                ),
+                round_info=TournamentRound(
+                    round=event["roundInfo"]["round"],
+                )
+            ) for event in response_data["events"]
+        ]
 
     def _map_response_to_tournament_season_rounds_model(self, response_data: UniqueTournamentSeasonRoundsResponse) -> dict[str, TournamentRound | list[TournamentRound]]:
         return {
--- src/domain/ports/tournament_repository.py
+++ src/domain/ports/tournament_repository.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+from domain.models.event import Event
 from domain.models.round import TournamentRound
 from domain.models.tournament import Tournament, TournamentSeason
 
@@ -18,4 +19,9 @@ class TournamentRepository(ABC):
     @abstractmethod
     async def get_tournament_rounds(self, tournament_id, season_id) -> dict[str, TournamentRound | list[TournamentRound]]:
         """Get tournament rounds by tournament id and season id."""
+        pass
+
+    @abstractmethod
+    async def get_tournament_round_events(self, tournament_id: int, season_id: int, round_number: int) -> list[Event]:
+        """Get tournament round events by tournament id, season id and round number."""
         pass
