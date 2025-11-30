f1a4f9927a2bbed2e977f84a73681f394d89141d
andreism <markoandreis07@gmail.com>
2025-11-30 10:16:20 +0100

    Implement service layer

--- src/services/tournament.py
+++ src/services/tournament.py
@@ -1,3 +1,4 @@
+from domain.models.event import Event
 from domain.models.round import TournamentRound
 from domain.models.tournament import Tournament, TournamentSeason
 from domain.ports.tournament_repository import TournamentRepository
@@ -15,3 +16,6 @@ class TournamentService:
 
     async def get_tournament_rounds(self, tournament_id, season_id) -> dict[str, TournamentRound | list[TournamentRound]]:
         return await self.tournament_repository.get_tournament_rounds(tournament_id, season_id)
+
+    async def get_tournament_round_events(self, tournament_id: int, season_id: int, round_number: int) -> list[Event]:
+        return await self.tournament_repository.get_tournament_round_events(tournament_id, season_id, round_number)
