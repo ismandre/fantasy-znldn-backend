88ccab07a9e85e16625e5e9ea9f3ed979f830d3e
andreism <markoandreis07@gmail.com>
2025-12-04 16:30:55 +0100

    Reformat clubs serializer

--- src/serializers/club.py
+++ src/serializers/club.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel, ConfigDict, Field
 
+
 class ClubSerializer(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     id: str
-    name: str = Field(description="The name of the club.", example="FC Test Club")
+    name: str = Field(description="The name of the club.", examples=["FC Test Club"])
