fb53cb8f924ba0224b3544e0f108182c6766dc2e
andreism <markoandreis07@gmail.com>
2025-12-04 16:29:30 +0100

    Reformat config

--- src/config.py
+++ src/config.py
@@ -2,15 +2,15 @@ from functools import lru_cache
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     API_HOST: str = "0.0.0.0"
     API_PORT: int = 9000
     API_RELOAD: bool = False
     API_PROXY_HEADERS: bool = True
 
-    model_config = SettingsConfigDict(
-        env_file=".env", env_file_encoding="utf-8"
-    )
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
 
 @lru_cache()
 def get_settings() -> Settings:
