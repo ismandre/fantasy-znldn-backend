16a59efe4b6b4dac921f22e2806a941ca55eb07c
andreism <markoandreis07@gmail.com>
2025-12-04 15:34:27 +0100

    Reformat tournament serializer

--- src/serializers/tournament.py
+++ src/serializers/tournament.py
@@ -2,10 +2,13 @@ from typing import Optional
 
 from pydantic import BaseModel, ConfigDict, Field
 
+
 class TournamentSerializer(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     id: int = Field(..., description="The ID of the tournament.")
     name: str = Field(description="The name of the tournament.")
     has_rounds: Optional[bool] = Field(description="Does the tournament have rounds?")
-    start_date: Optional[int] = Field(description="The timestamp of start date of the tournament.")
+    start_date: Optional[int] = Field(
+        description="The timestamp of start date of the tournament."
+    )
