94a2ead8730e2219b8c381cd6f5a5d0ddb3ce1bd
andreism <markoandreis07@gmail.com>
2025-12-04 15:59:02 +0100

    Reformat sofascore client

--- src/adapters/sofascore_client.py
+++ src/adapters/sofascore_client.py
@@ -1,7 +1,7 @@
 import json
-from typing import override
+from typing import Any, override
 
-from botasaurus.browser import browser, Driver
+from botasaurus.browser import Driver, browser  # type: ignore[import-not-found]
 
 from domain.ports.http_client import HttpClient
 
@@ -9,14 +9,14 @@ from domain.ports.http_client import HttpClient
 class SofascoreClient(HttpClient):
 
     @override
-    async def get(self, url):
+    async def get(self, url: str) -> Any:
         return botasaurus_browser_get_json(url)
 
 
-# from: https://github.com/oseymour/ScraperFC/blob/4af00054f5dd83c9e000c1d6c6de61bb6254d8ac/src/ScraperFC/utils/botasaurus_getters.py#L26
-@browser(headless=True, output=None, create_error_logs=False, block_images_and_css=True)
-def botasaurus_browser_get_json(driver: Driver, url: str) -> dict:
-    """ Use Botasaurus BROWSER model to get JSON from page
+# from: https://github.com/oseymour/ScraperFC/blob/4af00054f5dd83c9e000c1d6c6de61bb6254d8ac/src/ScraperFC/utils
+@browser(headless=True, output=None, create_error_logs=False, block_images_and_css=True)  # type: ignore[misc]
+def botasaurus_browser_get_json(driver: Driver, url: str) -> Any:
+    """Use Botasaurus BROWSER model to get JSON from page
 
     Parameters
     ----------
